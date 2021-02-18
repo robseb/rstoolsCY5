bcb39d08251edf0e021da4bbe640ceb742e596eb
robseb <git@robseb.de>
2021-02-18 10:16:21 +0000

    Test 123

---
+++ FPGA-readBridge/build/CMakeFiles/FPGA-readBridge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FPGA-readBridge.dir/main.o"
+  "FPGA-readBridge"
+  "FPGA-readBridge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FPGA-readBridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
