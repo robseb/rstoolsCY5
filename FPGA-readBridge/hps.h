60b846b95d90fa057bf64e0993a1a0e710a96a4a
Robin Sebastian <git@robseb.de>
2023-03-19 20:49:02 +0100

    [+] Bug fix with fd in hps.h

--- FPGA-readBridge/hps.h
+++ FPGA-readBridge/hps.h
@@ -70,11 +70,10 @@ extern "C"
     // macro to include the virtual Memory 
     //
     //
-    #define __VIRTUALMEM_SPACE_INIT()                                                                                          \
-            int fd;                                                                                                                \
-            fd = open("/dev/mem", (O_RDWR | O_SYNC));                                                                              \
-	    __hps_virtualAdreess_FPGAMFRDATA  = mmap(NULL, 0x04,(PROT_READ | PROT_WRITE), MAP_SHARED, fd, ALT_FPGAMGRDATA_OFST);      \
-	    __hps_virtualAdreess_FPGAMGR = mmap(NULL, 0x1000,(PROT_READ | PROT_WRITE), MAP_SHARED, fd, ALT_FPGAMGR_OFST)             
+    #define __VIRTUALMEM_SPACE_INIT()                                                                                          \                                                                                                            \
+        __fd = open("/dev/mem", (O_RDWR | O_SYNC));                                                                              \
+	    __hps_virtualAdreess_FPGAMFRDATA  = mmap(NULL, 0x04,(PROT_READ | PROT_WRITE), MAP_SHARED, __fd, ALT_FPGAMGRDATA_OFST);      \
+	    __hps_virtualAdreess_FPGAMGR = mmap(NULL, 0x1000,(PROT_READ | PROT_WRITE), MAP_SHARED, __fd, ALT_FPGAMGR_OFST)             
 
     #define __VIRTUALMEM_SPACE_DEINIT()                                                                                           \
             munmap((void*) __hps_virtualAdreess_FPGAMFRDATA, 0x04);                                                               \
