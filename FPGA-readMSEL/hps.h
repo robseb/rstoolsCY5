876748627b70e929151d867064836cb1c4060d45
Thomas Burghout <thomas.burghout@nedap.com>
2024-01-04 16:26:39 +0100

    Fix missing headers and trailing "

--- FPGA-readMSEL/hps.h
+++ FPGA-readMSEL/hps.h
@@ -70,7 +70,7 @@ extern "C"
     // macro to include the virtual Memory 
     //
     //
-    #define __VIRTUALMEM_SPACE_INIT()                                                                                          \                                                                                                          \
+    #define __VIRTUALMEM_SPACE_INIT()                                                                                          \
         __fd = open("/dev/mem", (O_RDWR | O_SYNC));                                                                              \
 	    __hps_virtualAdreess_FPGAMFRDATA  = mmap(NULL, 0x04,(PROT_READ | PROT_WRITE), MAP_SHARED, __fd, ALT_FPGAMGRDATA_OFST);      \
 	    __hps_virtualAdreess_FPGAMGR = mmap(NULL, 0x1000,(PROT_READ | PROT_WRITE), MAP_SHARED, __fd, ALT_FPGAMGR_OFST)             
